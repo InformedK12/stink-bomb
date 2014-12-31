861241984213d5aa6f4c73cbb857b1c95170321c
Robert Fletcher <lobatifricha@gmail.com>
2014-12-30 16:31:42 -0800

    set up configuration to handle bomb types
    Now the bomb is disabled by default.

--- lib/stink_bomb/configuration.rb
+++ lib/stink_bomb/configuration.rb
@@ -6,7 +6,28 @@ module StinkBomb
       self.message = options.fetch(:message) { default_message }
     end
 
+    def raise=(error_class)
+      remove_bomb(RaiseBomb)
+      if error_class == true
+        add_bomb(RaiseBomb.new)
+      elsif error_class
+        add_bomb(RaiseBomb.new(error_class: error_class))
+      end
+    end
+
+    def bombs
+      @bombs ||= []
+    end
+
   private
+
+    def add_bomb(bomb)
+      bombs << bomb
+    end
+
+    def remove_bomb(bomb_class)
+      bombs.delete_if { |bomb| bomb.is_a?(bomb_class) }
+    end
 
     def default_message
       'Your code stinks! It was supposed to be fixed by {time}'
---
+++ spec/lib/stink_bomb/configuration_spec.rb
@@ -0,0 +1,35 @@
+module StinkBomb
+  describe Configuration, '#bombs' do
+    let(:configuration) { StinkBomb::Configuration.new }
+
+    it 'returns an array of the currently assigned bombs' do
+      expect(configuration.bombs).to eq []
+      configuration.raise = true
+      expect(configuration.bombs.length).to eq 1
+    end
+  end
+
+  describe Configuration, '#raise=' do
+    let(:configuration) { StinkBomb::Configuration.new }
+
+    it 'removes any raise bombs when given false' do
+      configuration.bombs << RaiseBomb.new
+      configuration.raise = false
+      expect(configuration.bombs).to be_empty
+    end
+
+    it 'adds a bomb that raises a stinky error' do
+      configuration.raise = true
+      expect do
+        configuration.bombs.first.trigger('2011/01/01')
+      end.to raise_error(StinkyCodeError)
+    end
+
+    it 'adds a bomb that raises a specific error class when given' do
+      configuration.raise = StinkyTestError
+      expect do
+        configuration.bombs.first.trigger('2011/01/01')
+      end.to raise_error(StinkyTestError)
+    end
+  end
+end
